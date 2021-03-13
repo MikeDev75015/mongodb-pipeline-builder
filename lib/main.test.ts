d6a7ce770b0a59b4b0d40f4e097de61390d0a9b1
mikedev75015 <mikedev75015@gmail.com>
2021-03-13 15:43:01 +0100

    test: add first main unit tests

--- lib/main.test.ts
+++ lib/main.test.ts
@@ -1,1 +1,21 @@
+import { PipelineBuilder } from './main';
 
+describe('should create a new pipeline builder object', () => {
+    let pipelineBuilder: PipelineBuilder;
+
+    beforeEach(() => {
+        pipelineBuilder = new PipelineBuilder('test-the-builder');
+    });
+
+    describe('getName()', () => {
+        it('should return the pipeline builder name passed in the constructor', () => {
+            expect(pipelineBuilder.getName()).toEqual('test-the-builder');
+        });
+    });
+
+    describe('showDebug()', () => {
+        pipelineBuilder.showDebug();
+        expect(pipelineBuilder)
+    });
+
+});
