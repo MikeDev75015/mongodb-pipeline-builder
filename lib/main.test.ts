6bcba6687498c8e2ab316b49c7d29ab350308483
mikedev75015 <mikedev75015@gmail.com>
2021-03-13 20:05:59 +0100

    test: update the basic tests

--- lib/main.test.ts
+++ lib/main.test.ts
@@ -7,15 +7,40 @@ describe('should create a new pipeline builder object', () => {
         pipelineBuilder = new PipelineBuilder('test-the-builder');
     });
 
+    it('should be defined', () => {
+        expect(pipelineBuilder).toBeDefined();
+    });
+
     describe('getName()', () => {
         it('should return the pipeline builder name passed in the constructor', () => {
             expect(pipelineBuilder.getName()).toEqual('test-the-builder');
         });
     });
 
-    describe('showDebug()', () => {
-        pipelineBuilder.showDebug();
-        expect(pipelineBuilder)
+    describe('enableDebug()', () => {
+        it('should enable the builder debug', () => {
+            expect(pipelineBuilder.enableDebug()).toBeTruthy();
+        });
+    });
+
+    describe('disableDebug()', () => {
+        it('should disable the builder debug', () => {
+            expect(pipelineBuilder.disableDebug()).toBeFalsy();
+        });
+    });
+
+    describe('reset()', () => {
+        it('should return pipeline reset', () => {
+            expect(pipelineBuilder.resetPipeline()).toEqual([]);
+        });
+    });
+
+    describe('getDebugActionList()', () => {
+        it('should return the action stored in the debug history list at builder initialization', () => {
+            const debugActionList = pipelineBuilder.getDebugActionList();
+            expect(debugActionList).toHaveLength(1);
+            expect(debugActionList.map(d => d.action)).toEqual(['constructor']);
+        });
     });
 
 });
