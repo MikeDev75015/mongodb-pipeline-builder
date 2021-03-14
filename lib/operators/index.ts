5dfd38ebe8d95176fc7dc45f1661639c845a1be8
mikedev75015 <mikedev75015@gmail.com>
2021-03-14 19:46:38 +0100

    wip: prepare all pipeline operators

--- lib/operators/index.ts
+++ lib/operators/index.ts
@@ -125,14 +125,53 @@ export const Log10 = (num: number) => ({ $log10: num });
  * @constructor
  */
 export const Mod = (dividend: number, divisor: number) => ({ $mod: [ dividend, divisor ] });
-
-
-export const Multiply = () => ({  });	    // Multiplies numbers to return the product. Accepts any number of argument expressions.
-export const Pow = () => ({  });	        // Raises a number to the specified exponent.
-export const Round = () => ({  });	    // Rounds a number to to a whole integer or to a specified decimal place.
-export const Sqrt = () => ({  });	        // Calculates the square root.
-export const Subtract = () => ({  });	    // Returns the result of subtracting the second value from the first. If the two values are numbers, return the difference. If the two values are dates, return the difference in milliseconds. If the two values are a date and a number in milliseconds, return the resulting date. Accepts two argument expressions. If the two values are a date and a number, specify the date argument first as it is not meaningful to subtract a date from a number.
-export const Trunc = () => ({  });	    // Truncates a number to a whole integer or to a specified decimal place.
+/**
+ * Multiplies numbers to return the product. Accepts any number of argument expressions.
+ * @param args can be any valid expression as long as they resolve to numbers.
+ * @constructor
+ */
+export const Multiply = (...args: any[]) => ({ $multiply: args });
+/**
+ * Raises a number to the specified exponent.
+ * @param num can be any valid expression as long as it resolves to a number.
+ * @param exponent can be any valid expression as long as it resolves to a number.
+ * @constructor
+ */
+export const Pow = (num: number, exponent: number) => ({ $pow: [ num, exponent ] });
+/**
+ * Rounds a number to to a whole integer or to a specified decimal place.
+ * @param num can be any valid expression that resolves to a number. Specifically, the expression must resolve to an
+ * integer, double, decimal, or long.
+ * @param place can be any valid expression that resolves to an integer between -20 and 100, exclusive.
+ * e.g. -20 < place < 100. Defaults to 0 if unspecified.
+ * @constructor
+ */
+export const Round = (num: number, place?: number) => ({ $round : [ num, place ] });
+/**
+ * Calculates the square root of a positive number and returns the result as a double.
+ * @param num can be any valid expression as long as it resolves to a non-negative number.
+ * @constructor
+ */
+export const Sqrt = (num: number) => ({ $sqrt: num });
+/**
+ * Returns the result of subtracting the second value from the first. If the two values are numbers, return the difference.
+ * If the two values are dates, return the difference in milliseconds. If the two values are a date and a number in
+ * milliseconds, return the resulting date. Accepts two argument expressions. If the two values are a date and a number,
+ * specify the date argument first as it is not meaningful to subtract a date from a number.
+ * @param expression1 can be any valid expression as long as it resolves to a number and/or a date.
+ * @param expression2 can be any valid expression as long as it resolves to a number and/or a date.
+ * @constructor
+ */
+export const Subtract = (expression1: number, expression2: number) => ({ $subtract: [ expression1, expression2 ] });
+/**
+ * Truncates a number to a whole integer or to a specified decimal place.
+ * @param num can be any valid expression that resolves to a number. Specifically, the expression must resolve to an
+ * integer, double, decimal, or long.
+ * @param place Optional Can be any valid expression that resolves to an integer between -20 and 100, exclusive. e.g.
+ * -20 < place < 100. Defaults to 0 if unspecified.
+ * @constructor
+ */
+export const Trunc = (num: number, place?: number) => ({ $trunc : [ num, place ] });
 
 // Array Expression Operators
 
