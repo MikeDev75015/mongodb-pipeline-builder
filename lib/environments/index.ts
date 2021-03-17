f9bbdc8f5a8985eb82c2042de37f1eb4a8cf49f0
mikedev75015 <mikedev75015@gmail.com>
2021-03-17 22:10:39 +0100

    chore: group environment variables logs

--- lib/environments/index.ts
+++ lib/environments/index.ts
@@ -13,21 +13,11 @@ if (
 ) {
     console.log(
         'APP_ENV:', APP_ENV,
-        '\nLOGS_ENABLED:', LOGS_ENABLED === 'true'
+        '\nLOGS_ENABLED:', LOGS_ENABLED === 'true',
+        '\n\nMongoDB Database Config:' +
+        '\nBDD_USER:', BDD_USER,
+        '\nBDD_PASS:', BDD_PASS,
+        '\nBDD_CLUSTER:', BDD_CLUSTER,
+        '\nBDD_BASE:', BDD_BASE
     );
-
-    if (
-        BDD_USER &&
-        BDD_PASS &&
-        BDD_CLUSTER &&
-        BDD_BASE
-    ) {
-        console.log(
-            'MongoDB Database Config:' +
-            '\nBDD_USER:', BDD_USER,
-            '\nBDD_PASS:', BDD_PASS,
-            '\nBDD_CLUSTER:', BDD_CLUSTER,
-            '\nBDD_BASE:', BDD_BASE
-        );
-    }
 }
