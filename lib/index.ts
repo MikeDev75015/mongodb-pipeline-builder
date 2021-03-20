b3c47c31f38e56f0523b42f159db4d65a2465228
mikedev75015 <mikedev75015@gmail.com>
2021-03-20 02:47:10 +0100

    chore: update exports imports

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,3 @@
 export { PipelineBuilder } from './main';
 
-export { APP_ENV, LOGS_ENABLED,  BDD_USER, BDD_PASS, BDD_CLUSTER, BDD_BASE } from './environments';
+export { APP_ENV, APP_TIMEZONE, LOGS_ENABLED,  BDD_USER, BDD_PASS, BDD_CLUSTER, BDD_BASE } from './environments';
