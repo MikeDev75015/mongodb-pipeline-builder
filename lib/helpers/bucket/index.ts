32224d20e3420b80246cf096bbd9172d8b9037f4
mikedev75015 <mikedev75015@gmail.com>
2021-03-21 15:04:54 +0100

    chore: improve code quality

--- lib/helpers/bucket/index.ts
+++ lib/helpers/bucket/index.ts
@@ -4,26 +4,31 @@ import {BucketStageInterface} from "../../interfaces";
  * GroupBy Payload
  * @param groupBy
  * @param boundaries
- * @param default_id
+ * @param defaultId
  * @param output
  * @constructor
  */
 export const GroupByPayload = (
     groupBy: any,
     boundaries: any[],
-    default_id: string | number | null = "Other",
+    defaultId: string | number | null = "Other",
     output: any | null = { "count": { $sum: 1 }}
 ) => {
 
     const payload = {
         groupBy: groupBy,
         boundaries: boundaries,
-        default: default_id,
+        default: defaultId,
         output: output
     } as BucketStageInterface;
 
-    if (!default_id) delete payload.default;
-    if (!output) delete payload.output;
+    if (!defaultId) {
+        delete payload.default;
+    }
+
+    if (!output) {
+        delete payload.output;
+    }
 
     return payload;
 }
