7dacf5eb681258c53295bbeb9ee1b31f5836f3fb
mikedev75015 <mikedev75015@gmail.com>
2021-03-22 14:29:04 +0100

    test: update unit tests after payload refactor

--- lib/helpers/bucket/index.test.ts
+++ lib/helpers/bucket/index.test.ts
@@ -5,19 +5,12 @@ describe('bucket helpers', () => {
         test.each([
             [
                 GroupByPayload('$age', [6, 13, 18]),
-                { groupBy: '$age', boundaries: [6, 13, 18], default: "Other", output: { "count": { $sum: 1 } } }
-            ],
-            [
-                GroupByPayload('$age', [20, 30], 19, { "ages": { $push: '$age' } }),
-                { groupBy: '$age', boundaries: [20, 30], default: 19, output: { "ages": { $push: '$age' } } }
-            ],
-            [
-                GroupByPayload('$age', [6, 13, 18], null),
                 { groupBy: '$age', boundaries: [6, 13, 18], output: { "count": { $sum: 1 } } }
             ],
             [
-                GroupByPayload('$age', [20, 30], null, null),
-                { groupBy: '$age', boundaries: [20, 30] }
+                GroupByPayload('$age', [20, 30],
+                    { output: { "ages": { $push: '$age' } }, default: 'test' }),
+                { groupBy: '$age', boundaries: [20, 30], default: 'test', output: { "ages": { $push: '$age' } } }
             ],
         ])('%o should return %o', (
             operation: any,
