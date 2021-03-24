af4010b89c0d71cc37ac15de4d38429529a91002
mikedev75015 <mikedev75015@gmail.com>
2021-03-24 11:25:17 +0100

    wip: add GetResult super method

---
+++ lib/methods/index.ts
@@ -0,0 +1,40 @@
+import {StageInterface} from "../interfaces";
+import {PipelineError} from "../errors";
+
+/**
+ * Apply the aggregate method on the chosen target
+ * @param target The collection or the mongoose model where to apply the pipeline. The target must have the aggregate
+ * method
+ * @param pipeline The pipeline to apply to the target
+ * @constructor
+ */
+export const GetResult = async (target: any, pipeline: StageInterface[]) => {
+    if (!target) {
+        throw new PipelineError('Application not possible, the target is not valid.');
+    }
+    if (!target.aggregate) {
+        throw new PipelineError('Application not possible, the aggregate method does not exist on the chosen target.');
+    }
+    if (!pipeline.length) {
+        throw new PipelineError('Application not possible, the pipeline is empty.');
+    }
+
+    const result = await target.aggregate(pipeline);
+
+    // Paging result
+    if (result.length === 1 && result[0].docs && result[0].count && result[0].count[0]) {
+
+        return {
+            GetDocs: () => result[0].docs,
+            GetCount: () => result[0].count[0].totalElements
+        }
+    }
+
+    // Default result
+    return {
+        GetDocs: () => result,
+        GetCount: () => result.length
+    };
+}
+
+
