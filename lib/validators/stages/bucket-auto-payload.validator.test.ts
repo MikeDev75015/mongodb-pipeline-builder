2c259616db777323191d09aff57e1259d92e4c51
mikedev75015 <mikedev75015@gmail.com>
2021-03-25 13:02:36 +0100

    test: add or update some validators unit tests

--- lib/validators/stages/bucket-auto-payload.validator.test.ts
+++ lib/validators/stages/bucket-auto-payload.validator.test.ts
@@ -19,7 +19,7 @@ describe('bucket auto validators', () => {
             [bucketAutoPayloadValidator(payloadList[2] as BucketAutoStageInterface),
                 'The groupBy property is required.'],
             [bucketAutoPayloadValidator(payloadList[3] as BucketAutoStageInterface),
-                'The buckets property is required.'],
+                'The buckets value is not valid. You must specify a positive 32-bit integer.'],
             [bucketAutoPayloadValidator(payloadList[4] as BucketAutoStageInterface),
                 'The granularity value is not valid. You must specify one of these possible values: R5 | R10 | R20 | R40 | R80 | 1-2-5 | E6 | E12 | E24 | E48 | E96 | E192 | POWERSOF2.'],
             [bucketAutoPayloadValidator(payloadList[5] as BucketAutoStageInterface),
