73a25b92dd09bab952404861049be328f87f6184
mikedev75015 <mikedev75015@gmail.com>
2021-03-26 02:52:29 +0100

    chore: improve facetPayloadValidator unit tests coverage

--- lib/validators/stages/facet-payload.validator.test.ts
+++ lib/validators/stages/facet-payload.validator.test.ts
@@ -5,8 +5,11 @@ describe('facet validators', () => {
         const payloadList: any[] = [
             {},
             { out1: { tests: 'unit' } },
+            { out1multi1: { tests: 'unit' }, out1multi2: { tests: 'unit' } },
             { out2: [{ $tests: 'unit' }] },
+            { out2multi1: [{ $tests: 'unit' }, { $name: 'toto' }], out2multi2: [{ $tests: 'unit' }] },
             { out3: [{ $facet: 'unit' }] },
+            { out3multi1: [{ $facet: 'unit' }], out3multi2: [{ $merge: 'unit' }] },
             { out4: [{ $count: 'unit' }] },
         ];
         test.each([
@@ -15,10 +18,16 @@ describe('facet validators', () => {
             [facetPayloadValidator(payloadList[1]),
                 'The out1 pipeline is not valid.'],
             [facetPayloadValidator(payloadList[2]),
-                'The out2 output contains one or more invalid stages.'],
+                'The out1multi1 / out1multi2 pipelines are not valid.'],
             [facetPayloadValidator(payloadList[3]),
-                'The out3 output contains one or more incompatible stages.'],
+                'The out2 output contains one or more invalid stages.'],
             [facetPayloadValidator(payloadList[4]),
+                'The out2multi1 / out2multi2 outputs contains one or more invalid stages.'],
+            [facetPayloadValidator(payloadList[5]),
+                'The out3 output contains one or more incompatible stages.'],
+            [facetPayloadValidator(payloadList[6]),
+                'The out3multi1 / out3multi2 outputs contains one or more incompatible stages.'],
+            [facetPayloadValidator(payloadList[7]),
                 ''],
         ])('%o should return %s', (
             operation: any,
