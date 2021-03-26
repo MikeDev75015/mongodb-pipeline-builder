3d508f8ce448870830eae55f7dbe056d5d43dae8
mikedev75015 <mikedev75015@gmail.com>
2021-03-26 02:37:56 +0100

    chore: add some validators

---
+++ lib/validators/stages/group-payload.validator.ts
@@ -0,0 +1,10 @@
+import {GroupStageInterface} from "../../interfaces";
+
+/**
+ * Checks the presence of mandatory fields and the validity of each field present in the payload
+ * @param payload The value passed to the stage
+ * @returns an error message if non-compliant, an empty string if compliant
+ */
+export const groupPayloadValidator = (payload: GroupStageInterface) => {
+    return '';
+}
