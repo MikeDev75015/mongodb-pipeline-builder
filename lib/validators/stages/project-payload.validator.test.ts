3db1bebd3a78e264edc783a429b3de270901e330
mikedev75015 <mikedev75015@gmail.com>
2021-03-27 16:48:13 +0100

    test: add or update some validators unit tests

--- lib/validators/stages/project-payload.validator.test.ts
+++ lib/validators/stages/project-payload.validator.test.ts
@@ -3,11 +3,16 @@ import {projectPayloadValidator} from "./project-payload.validator";
 describe('project validators', () => {
     describe('projectPayloadValidator', () => {
         const payloadList: any[] = [
-            {}
+            {},
+            { tests: undefined },
+            { tests: 'unit' }
         ];
         test.each([
             [projectPayloadValidator(payloadList[0]),
-                ''],
+                'No fields have been added.'],
+            [projectPayloadValidator(payloadList[1]),
+                'One or more field values are not defined.'],
+            [projectPayloadValidator(payloadList[2]), ''],
         ])('%o should return %s', (
             operation: any,
             expected: any
