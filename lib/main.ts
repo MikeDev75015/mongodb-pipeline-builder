df4002a80314aa87093e6de89e4f48b83f47e2ac
mikedev75015 <mikedev75015@gmail.com>
2021-04-07 00:06:35 +0200

    chore: Remove the unused import of FacetStageInterface

--- lib/main.ts
+++ lib/main.ts
@@ -5,7 +5,6 @@ import {
     BuilderOptionsInterface,
     CollStatsStageInterface,
     DebugBuildInterface,
-    FacetStageInterface,
     GeoNearStageInterface,
     getStageTypeValue,
     GraphLookupStageInterface,
